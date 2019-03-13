4c2a0f25035d66064589f7151ab81c056a883c0d
markupus_ksenia <ksenia@markupus.tech>
2019-03-13 12:26:37 +0200

    update fetch request for btns

--- main.js
+++ main.js
@@ -1,28 +1,33 @@
+let filterGallery = addElement('section');
+let filter = addElement('div', filterGallery);
+filter.className = "filter-holder";
+let filterButton;
+
 function addElement(childElem, parentElem) {
     return (parentElem ? parentElem : document.body).appendChild(
         document.createElement(childElem)
     )
 }
 
-let filterGallery = addElement('section');
-let filter = addElement('div', filterGallery);
-filter.className = "filter-holder";
-let filterButton;
-
-let categoryContent = 'categorys.json';
-
-
-function test (url) {
-     fetch(url)
-        .then(response => response.json()
-             .then(response => {
-                 response.forEach(
-                     item => {
-                         filterButton = addElement('button', filter);
-                         filterButton.innerText = item;
-                     }
-                 )
-             }))
+let getData = function (ref) {
+    return fetch('http://localhost:3000/' + ref)
+        .then(response => response.json())
 };
 
-test(categoryContent);
+let createButtons = function () {
+    new Promise (
+        resolve => {
+            getData('categories').then(data => {
+                data.forEach(
+                    item => {
+                        filterButton = addElement('button', filter);
+                        filterButton.innerText = item;
+                    }
+                )
+            })
+        }
+
+    )
+};
+
+createButtons();
